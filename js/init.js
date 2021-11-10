5a01f81963a7e07e2d459531a1685d012a237720
gagfe3 <94073986+gagfe3@users.noreply.github.com>
2021-11-10 12:53:24 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCaExaeJUnIVAx4tOajeaxCnsEkAwg8jD0",
+  authDomain: "salinaspk13.firebaseapp.com",
+  projectId: "salinaspk13",
+  storageBucket: "salinaspk13.appspot.com",
+  messagingSenderId: "453647372950",
+  appId: "1:453647372950:web:8dd15beecc6be0ae69a8b7",
+  measurementId: "G-90Z3LQXR8M"
+    });
